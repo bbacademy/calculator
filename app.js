eaa612e048c64be31ca181a1dfd2c658f47aecfe
Phong Vu <phong.vu@me.com>
2021-09-16 18:40:32 +0700

    update code

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+const output = document.querySelector('output')
 
+const fn = {
+	add: (a, b) => (a + b),
+	substract: (a, b) => (a - b),
+	multiply: (a, b) => (a * b),
+	divide: (a, b) => (a / b),
+	power: (a, b) => Math.pow(a, b),
+	percent: (a, b) => (Math.round(a / b * 100)),
+}
+const store = { value: 0, method: '' }
+
+addEventListener('click', ({ target }) => {
+	if (target.matches('input')) {
+		output.value += target.value
+	}
+	if (target.matches('button')) {
+		switch (target.name) {
+			case 'negate':
+				output.value = parseInt(output.value) * -1
+				break
+			case 'clear':
+				output.value = ''
+				store.value = undefined
+				store.method = undefined
+				break
+			case 'equal':
+				if (store.method)
+					output.value = store.method(store.value, parseInt(output.value))
+				break
+			default:
+				store.method = fn[target.name]
+				store.value = parseInt(output.value)
+				output.value = ''
+		}
+	}
+})
